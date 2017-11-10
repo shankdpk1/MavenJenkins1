9727cc9be7a6bb574a5e015ba5f32aa6a7ce82dd
shankdpk1 <33471809+shankdpk1@users.noreply.github.com>
2017-11-10 17:17:05 -0500

    Add files via upload

---
+++ testNG.bat
@@ -0,0 +1,2 @@
+cd C:\Users\shash\eclipse-workspace\SeleniumLearning\bin\testNGPrintMsg
+java -cp C:\Users\shash\eclipse-workspace\JarFiles\TestNGJars\*;C:\Users\shash\eclipse-workspace\SeleniumLearning\bin\ org.testng.TestNG testng.xml
